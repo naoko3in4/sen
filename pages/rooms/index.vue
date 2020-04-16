f81e1fafe76d56b64605c75804a642e085a84a01
Naoko3in4 <okashimochi@gimail.com>
2020-04-16 19:21:22 +0900

    added a modal for creating rooms

--- pages/rooms/index.vue
+++ pages/rooms/index.vue
@@ -10,11 +10,32 @@
           <v-card-title class="headline">
             {{ room.room_name }}
           </v-card-title>
-          <!-- <v-card-actions>
-            <v-spacer />
-          </v-card-actions> -->
         </v-card>
       </div>
+      <!-- modal -->
+      <v-row justify="center">
+        <v-dialog v-model="dialog" persistent max-width="290">
+          <template v-slot:activator="{ on }">
+            <v-row justify="space-around">
+              <v-btn color="primary" dark class="modal-trigger" v-on="on"
+                >+Add</v-btn
+              >
+            </v-row>
+          </template>
+          <v-card>
+            <v-card-title class="headline">ルームを追加する</v-card-title>
+            <v-card-text>
+              <label for="名前">項目</label>
+              <p><input type="text" placeholder="ルーム名を入力" /></p>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="dialog = false">やめる</v-btn>
+              <v-btn color="primary" text @click="dialog = false">登録</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog> </v-row
+      ><!-- modal -->
     </v-flex>
   </v-layout>
 </template>
@@ -24,6 +45,11 @@ import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'Room',
+  data() {
+    return {
+      dialog: false
+    }
+  },
   computed: {
     ...mapState('room', ['feedRooms']),
     getrooms() {
