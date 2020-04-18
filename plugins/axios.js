1cee6178e52c4138a4ab4b33506367e7f79f9ff2
Naoko3in4 <okashimochi@gimail.com>
2020-04-18 19:10:45 +0900

    fixed axios headers

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-axios.defaults.headers.common['X-API-KEY'] = "MICROCMS_GET_APIKEY=ddc2c6b9-fac7-4ce5-85a1-3f44b75cc2b8"
-axios.defaults.headers.common['X-WRITE-API-KEY'] = "MICROCMS_POST_APIKEY=944297e4-faf2-4548-b139-822733e2450e"
+axios.defaults.headers.common['X-API-KEY'] = "ddc2c6b9-fac7-4ce5-85a1-3f44b75cc2b8"
+axios.defaults.headers.common['X-WRITE-API-KEY'] = "944297e4-faf2-4548-b139-822733e2450e"
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 export default axios
